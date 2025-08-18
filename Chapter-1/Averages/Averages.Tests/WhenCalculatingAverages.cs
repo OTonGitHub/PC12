a4e79a1d1320a35525ed785056d974c1bce381d8
purple@daraknet.cc <purple@daraknet.cc>
2025-08-18 18:37:19 +0500

    init commit

---
+++ Chapter-1/Averages/Averages.Tests/WhenCalculatingAverages.cs
@@ -0,0 +1,12 @@
+namespace Averages.Tests;
+
+[TestClass]
+public class WhenCalculatingAverages
+{
+    [TestMethod]
+    public void SingleInputShouldProduceSameValueAsResult()
+    {
+        // string[] inputs = { "1" };
+        // double result = A
+    }
+}
