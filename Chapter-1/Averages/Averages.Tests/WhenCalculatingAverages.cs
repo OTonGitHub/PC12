a8782ca4a9fdc000f768ed50f8d336c4072a2e11
purple@daraknet.cc <purple@daraknet.cc>
2025-08-19 09:32:41 +0500

    add initial skeletons

--- Chapter-1/Averages/Averages.Tests/WhenCalculatingAverages.cs
+++ Chapter-1/Averages/Averages.Tests/WhenCalculatingAverages.cs
@@ -1,12 +1,26 @@
 namespace Averages.Tests;
 
+
 [TestClass]
 public class WhenCalculatingAverages
+/*
+    "A class for each scenario in which you want to test a particular class, 
+    with one method for each of the things that should be true about your code in that scenario."
+*/
 {
     [TestMethod]
     public void SingleInputShouldProduceSameValueAsResult()
     {
-        // string[] inputs = { "1" };
-        // double result = A
+        string[] inputs = { "1" };
+        double result = AverageCalculator.ArthemeticMean(inputs);
+        Assert.AreEqual(1.0, result, 1E-14);
+    }
+
+    [TestMethod]
+    public void MultipleInputsShouldProduceAverageAsResult()
+    {
+        string[] inputs = { "1", "2", "3" };
+        double result = AverageCalculator.ArthemeticMean(inputs);
+        Assert.AreEqual(2.0, result, 1E-14);
     }
 }
---
+++ Chapter-1/Averages/Averages/AverageCalculator.cs
@@ -0,0 +1,9 @@
+namespace Averages;
+
+public static class AverageCalculator
+{
+    public static double ArthemeticMean(string[] args)
+    {
+        return 1.0;
+    }
+}
